ff1e2f409ea8922dcc55bafd09edf72d22fea261
LolloDev <andreapascullita@Gmail.com>
2021-07-27 23:44:25 +0200

    Update GetLastBlock.lua

--- GetLastBlock.lua
+++ GetLastBlock.lua
@@ -3,7 +3,7 @@
 --]]
 
 local hs = game:GetService("HttpService")
-local url = "http://btc.blockr.io/api/v1/block/info/last"
+local url = "https://api-r.bitcoinchain.com/v1/blocks/10"
 
 return function()
 	local data = hs:GetAsync(url, true)
